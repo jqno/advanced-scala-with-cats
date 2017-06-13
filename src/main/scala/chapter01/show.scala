0cd0e55892adcac95dfc80e0d1f4ed852b02d8e9
Jan Ouwens <jan.ouwens@gmail.com>
2017-06-13 08:59:25 +0200

    Exercise 1.2.5: Cat Show

---
+++ src/main/scala/chapter01/show.scala
@@ -0,0 +1,16 @@
+package chapter01
+
+import cats.Show
+import cats.instances.all._
+import cats.syntax.show._
+
+object ShowInstances {
+  implicit val showCat = new Show[Cat] {
+    override def show(f: Cat): String = {
+      val name = f.name.show
+      val age = f.age.show
+      val color = f.color.show
+      s"$name is a $age year old $color cat."
+    }
+  }
+}
---
+++ src/test/scala/chapter01/ShowTest.scala
@@ -0,0 +1,26 @@
+package chapter01
+
+import cats.instances.all._
+import cats.syntax.show._
+import chapter01.ShowInstances._
+import org.scalatest.{FlatSpec, Matchers}
+
+class ShowTest extends FlatSpec with Matchers {
+
+  behavior of "Show"
+
+  it should "format a String" in {
+    // Can't use pretty syntax here,
+    // because String already has a format method which conflicts.
+    "string".show should be ("string")
+  }
+
+  it should "format an Int" in {
+    42.show should be ("42")
+  }
+
+  it should "format a Cat" in {
+    val cat = Cat("Sammie", 10, "brown")
+    cat.show should be ("Sammie is a 10 year old brown cat.")
+  }
+}
