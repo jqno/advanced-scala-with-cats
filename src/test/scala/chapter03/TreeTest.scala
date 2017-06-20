659ba35432c4b8f9f6506c34720ccff656e3ebe7
Jan Ouwens <jan.ouwens@gmail.com>
2017-06-20 07:40:19 +0200

    Exercise 3.5.4: Branching out with Functors

---
+++ src/test/scala/chapter03/TreeTest.scala
@@ -0,0 +1,25 @@
+package chapter03
+
+import cats.syntax.functor._
+import org.scalatest.{FlatSpec, Matchers}
+
+class TreeTest extends FlatSpec with Matchers {
+
+  behavior of "Tree functor"
+
+  val tree: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+
+  it should "behave mappily" in {
+    tree map (_ + 1) should be (Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))
+  }
+
+  it should "follow the identity law" in {
+    tree map identity should be (tree)
+  }
+
+  it should "follow the composition law" in {
+    val first = tree map (_ * 2 + 1)
+    val second = tree map (_ * 2) map (_ + 1)
+    first should be (second)
+  }
+}
