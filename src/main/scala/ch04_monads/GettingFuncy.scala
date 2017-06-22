5625d395c726dd19ed7c99021afdbbd58b5c3662
Jan Ouwens <jan.ouwens@gmail.com>
2017-06-22 07:55:21 +0200

    Exercise 4.1.2: Getting Func-y

---
+++ src/main/scala/ch04_monads/GettingFuncy.scala
@@ -0,0 +1,13 @@
+package ch04_monads
+
+import scala.language.higherKinds
+
+object GettingFuncy {
+  trait Monad[F[_]] {
+    def pure[A](a: A): F[A]
+    def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
+
+    def map[A, B](value: F[A])(func: A => B): F[B] =
+      flatMap(value)(func andThen pure)
+  }
+}
