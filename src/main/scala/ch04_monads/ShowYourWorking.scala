bfedd00e926997ebe5598588f8877e5e50ab9683
Jan Ouwens <jan.ouwens@gmail.com>
2017-06-27 07:53:51 +0200

    Exercise 4.6.3: what I came up with (it doesn't compile)

--- src/main/scala/ch04_monads/ShowYourWorking.scala
+++ src/main/scala/ch04_monads/ShowYourWorking.scala
@@ -1,17 +1,34 @@
 package ch04_monads
 
+import cats.data.Writer
+import cats.syntax.applicative._
+import cats.syntax.writer._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationLong
 import scala.concurrent.{Await, Future}
 
 object ShowYourWorking extends App {
 
+  type Logged[A] = Writer[Vector[String], A]
+
   def slowly[A](body: => A): A =
     try body finally Thread.sleep(100)
 
   def factorial(n: Int): Int = {
-    val ans = slowly(if (n == 0) 1 else n * factorial(n - 1))
-    println(s"fact $n $ans")
+    def loop(logged: Logged[Int]): Logged[Int] = {
+      slowly {
+        for {
+          n <- logged
+          prev <- loop((n - 1).pure[Logged])
+          ans = n * prev
+          _ <- Vector(s"fact $n $ans").tell
+        } yield ans
+      }
+    }
+
+    val (log, ans) = loop(n.pure[Logged]).run
+    log.foreach(println)
     ans
   }
 
