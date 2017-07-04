998303d8582f0af5df007a8240c36dd30220b661
Jan Ouwens <jan.ouwens@gmail.com>
2017-07-04 07:58:58 +0200

    Exercise 4.7.3: methods

--- src/main/scala/ch04_monads/HackingOnReaders.scala
+++ src/main/scala/ch04_monads/HackingOnReaders.scala
@@ -6,4 +6,10 @@ object HackingOnReaders {
   case class Db(usernames: Map[Int, String], passwords: Map[String, String])
 
   type DbReader[A] = Reader[Db, A]
+
+  def findUsername(userId: Int): DbReader[Option[String]] =
+    Reader(_.usernames.get(userId))
+
+  def checkPassword(username: String, password: String): DbReader[Boolean] =
+    Reader(_.passwords.get(username).contains(password))
 }
