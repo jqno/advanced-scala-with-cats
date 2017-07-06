dc7e7c0249d0ea1c2688e613bc2e7f79eac9adbf
Jan Ouwens <jan.ouwens@gmail.com>
2017-07-06 07:59:25 +0200

    Exercise 4.9.1: add tests

---
+++ src/test/scala/ch04_monads/BranchingOutFurtherTest.scala
@@ -0,0 +1,45 @@
+package ch04_monads
+
+import ch03_functors.{Branch, Leaf, Tree}
+import ch04_monads.BranchingOutFurther.treeMonad
+import org.scalatest.{FlatSpec, Matchers}
+
+class BranchingOutFurtherTest extends FlatSpec with Matchers {
+
+  def duplicate[A](a: A): Tree[A] =
+    Branch(Leaf(a), Leaf(a))
+
+  val someLeaf = Leaf(1)
+  val someBranch = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
+
+
+  behavior of "TreeMonad"
+
+  it should "work on Leaf" in {
+    val expected = Branch(Leaf(1), Leaf(1))
+    val actual = treeMonad.flatMap(someLeaf)(duplicate)
+    actual should be (expected)
+  }
+
+  it should "work on Branch" in {
+    val expected = Branch(Branch(Leaf(1), Leaf(1)), Branch(Branch(Leaf(2), Leaf(2)), Branch(Leaf(3), Leaf(3))))
+    val actual = treeMonad.flatMap(someBranch)(duplicate)
+    actual should be (expected)
+  }
+
+  it should "have Functor-like behaviour" in {
+    val expected = Branch(Leaf(2), Branch(Leaf(3), Leaf(4)))
+    val actual = treeMonad.map(someBranch)(_ + 1)
+    actual should be (expected)
+  }
+
+  it should "support for-comprehensions when the monad is in the implicit scope" in {
+    import cats.syntax.all._
+    implicit val tm = treeMonad
+    val t = for {
+      a <- someLeaf: Tree[Int]
+      b <- someBranch: Tree[Int]
+    } yield a + b
+    t should be (Branch(Leaf(2), Branch(Leaf(3), Leaf(4))))
+  }
+}
