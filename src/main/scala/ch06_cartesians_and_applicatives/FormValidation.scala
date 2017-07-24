b9a7db2c914ffbb38595cf0421caee8d79e91c53
Jan Ouwens <jan.ouwens@gmail.com>
2017-07-24 07:34:31 +0200

    Exercise 6.4.4: parseInt

--- src/main/scala/ch06_cartesians_and_applicatives/FormValidation.scala
+++ src/main/scala/ch06_cartesians_and_applicatives/FormValidation.scala
@@ -1,10 +1,17 @@
 package ch06_cartesians_and_applicatives
 
+import scala.util.Try
+
 object FormValidation {
 
   type FormData = Map[String, String]
   type ErrorsOr[A] = Either[List[String], A]
 
-  def getValue(map: Map[String, String], fieldName: String): ErrorsOr[String] =
+  def getValue(map: FormData, fieldName: String): ErrorsOr[String] =
     map.get(fieldName).toRight(List(s"Field name $fieldName not specified"))
+
+  def parseInt(map: FormData, fieldName: String): ErrorsOr[Int] =
+    getValue(map, fieldName).right.flatMap { v =>
+      Try(v.toInt).toOption.toRight(List(s"Value $v in $fieldName was not an int"))
+    }
 }
--- src/test/scala/ch06_cartesians_and_applicatives/FormValidationSpec.scala
+++ src/test/scala/ch06_cartesians_and_applicatives/FormValidationSpec.scala
@@ -7,6 +7,7 @@ class FormValidationSpec extends FlatSpec with Matchers {
 
   val someMap = Map("name" -> "Pietje", "age" -> "42")
 
+
   behavior of "getValue"
 
   it should "get the value of an existing key" in {
@@ -15,5 +16,16 @@ class FormValidationSpec extends FlatSpec with Matchers {
 
   it should "give an error message when the fieldName does not exist" in {
     getValue(someMap, "invalid") should be (Left(List("Field name invalid not specified")))
+  }
+
+
+  behavior of "parseInt"
+
+  it should "parse an int value" in {
+    parseInt(someMap, "age") should be (Right(42))
+  }
+
+  it should "give an error message when the field is not an int" in {
+    parseInt(someMap, "name") should be (Left(List("Value Pietje in name was not an int")))
   }
 }
