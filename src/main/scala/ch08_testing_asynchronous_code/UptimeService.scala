2141d5b4b21ab96a755daa97e591e28295c32c26
Jan Ouwens <jan.ouwens@gmail.com>
2017-07-25 16:48:42 +0200

    Chapter 8: UptimeService implementation

--- src/main/scala/ch08_testing_asynchronous_code/UptimeService.scala
+++ src/main/scala/ch08_testing_asynchronous_code/UptimeService.scala
@@ -1,8 +1,13 @@
 package ch08_testing_asynchronous_code
 
+import cats.Applicative
+import cats.instances.list._
+import cats.syntax.functor._
+import cats.syntax.traverse._
+
 import scala.language.higherKinds
 
-class UptimeService[F[_]](client: UptimeClient[F]) {
+class UptimeService[F[_]: Applicative](client: UptimeClient[F]) {
   def getTotalUptime(hostnames: List[String]): F[Int] =
-    ???
+    hostnames.traverse(client.getUptime).map(_.sum)
 }
