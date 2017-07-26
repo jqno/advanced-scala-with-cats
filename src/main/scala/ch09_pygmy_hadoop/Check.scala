e72ee0995983024b8e73ce1831b97ad39a2ad2ac
Jan Ouwens <jan.ouwens@gmail.com>
2017-07-26 16:48:37 +0200

    Chapter 10: simplify apply (peeked from book)

--- src/main/scala/ch09_pygmy_hadoop/Check.scala
+++ src/main/scala/ch09_pygmy_hadoop/Check.scala
@@ -2,18 +2,14 @@ package ch09_pygmy_hadoop
 
 import cats.Semigroup
 import cats.data.Validated
-import cats.data.Validated.{Invalid, Valid}
-import cats.syntax.semigroup._
+import cats.syntax.cartesian._
 
 sealed trait Check[E, A] {
   def apply(value: A)(implicit ev: Semigroup[E]): Validated[E, A] = this match {
-    case Pure(f) => f(value)
-    case And(c1, c2) => (c1(value), c2(value)) match {
-      case (Valid(_), Valid(_)) => Valid(value)
-      case (left, Valid(_)) => left
-      case (Valid(_), left) => left
-      case (Invalid(e1), Invalid(e2)) => Invalid(e1 combine e2)
-    }
+    case Pure(f) =>
+      f(value)
+    case And(c1, c2) =>
+      (c1(value) |@| c2(value)).map((_, _) => value)
   }
 
   def and(that: Check[E, A]): Check[E, A] =
