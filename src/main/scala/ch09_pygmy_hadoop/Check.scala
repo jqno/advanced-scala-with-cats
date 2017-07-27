0b0a2357111fad6ef92ce44b81e7eb2243f3377d
Jan Ouwens <jan.ouwens@gmail.com>
2017-07-27 16:36:00 +0200

    Chapter 10: andThen

--- src/main/scala/ch09_pygmy_hadoop/Check.scala
+++ src/main/scala/ch09_pygmy_hadoop/Check.scala
@@ -2,6 +2,7 @@ package ch09_pygmy_hadoop
 
 import cats.Semigroup
 import cats.data.Validated
+import cats.data.Validated.{Invalid, Valid}
 import cats.syntax.either._
 
 sealed trait Check[E, A, B] {
@@ -12,6 +13,9 @@ sealed trait Check[E, A, B] {
 
   def flatMap[C](f: B => Check[E, A, C]): Check[E, A, C] =
     FlatMap(this, f)
+
+  def andThen[C](that: Check[E, B, C]): Check[E, A, C] =
+    AndThen(this, that)
 }
 
 object Check {
@@ -32,4 +36,12 @@ case class Map[E, A, B, C](c: Check[E, A, B], f: B => C) extends Check[E, A, C] 
 case class FlatMap[E, A, B, C](c: Check[E, A, B], f: B => Check[E, A, C]) extends Check[E, A, C] {
   override def apply(value: A)(implicit ev: Semigroup[E]): Validated[E, C] =
     c(value).withEither(_.flatMap(b => f(b)(value).toEither))
+}
+
+case class AndThen[E, A, B, C](c1: Check[E, A, B], c2: Check[E, B, C]) extends Check[E, A, C] {
+  override def apply(value: A)(implicit ev: Semigroup[E]): Validated[E, C] =
+    c1(value) match {
+      case Valid(b) => c2(b)
+      case Invalid(e) => Invalid(e)
+    }
 }
