5d55a233e5e12a83f619497175cc448c1e36c755
Jan Ouwens <jan.ouwens@gmail.com>
2017-07-27 16:26:18 +0200

    Chapter 10: flatMap

--- src/main/scala/ch09_pygmy_hadoop/Check.scala
+++ src/main/scala/ch09_pygmy_hadoop/Check.scala
@@ -2,12 +2,16 @@ package ch09_pygmy_hadoop
 
 import cats.Semigroup
 import cats.data.Validated
+import cats.syntax.either._
 
 sealed trait Check[E, A, B] {
   def apply(value: A)(implicit ev: Semigroup[E]): Validated[E, B]
 
   def map[C](f: B => C): Check[E, A, C] =
     Map(this, f)
+
+  def flatMap[C](f: B => Check[E, A, C]): Check[E, A, C] =
+    FlatMap(this, f)
 }
 
 object Check {
@@ -23,4 +27,9 @@ case class PureCheck[E, A](p: Predicate[E, A]) extends Check[E, A, A] {
 case class Map[E, A, B, C](c: Check[E, A, B], f: B => C) extends Check[E, A, C] {
   override def apply(value: A)(implicit ev: Semigroup[E]): Validated[E, C] =
     c(value).map(f)
+}
+
+case class FlatMap[E, A, B, C](c: Check[E, A, B], f: B => Check[E, A, C]) extends Check[E, A, C] {
+  override def apply(value: A)(implicit ev: Semigroup[E]): Validated[E, C] =
+    c(value).withEither(_.flatMap(b => f(b)(value).toEither))
 }
