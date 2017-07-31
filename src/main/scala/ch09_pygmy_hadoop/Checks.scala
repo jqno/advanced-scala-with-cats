73a5e8ff66be746a855522ddec4e2ac29ef16ff0
Jan Ouwens <jan.ouwens@gmail.com>
2017-07-31 16:57:03 +0200

    Chapter 10: Checks

--- src/main/scala/ch09_pygmy_hadoop/Checks.scala
+++ src/main/scala/ch09_pygmy_hadoop/Checks.scala
@@ -1,9 +1,20 @@
 package ch09_pygmy_hadoop
 
-import ch09_pygmy_hadoop.Predicates.Errors
+import cats.instances.all._
+import cats.syntax.all._
+import ch09_pygmy_hadoop.Predicates._
 
 object Checks {
 
-  val usernameCheck: Check[Errors, String, String] = ???
-  val emailCheck: Check[Errors, String, String] = ???
+  val usernameCheck: Check[Errors, String, String] = Check {
+    longerThan(3) and alphanumeric
+  }
+
+  val emailCheck: Check[Errors, String, String] =
+    Check(contains('@')).flatMap { s =>
+      val (left :: right :: Nil) = s.split('@').toList
+      val l = Check(longerThan(0))(left)
+      val r = Check(longerThan(2) and contains('.'))(right)
+      Check(_ => l |+| r)
+    }
 }
