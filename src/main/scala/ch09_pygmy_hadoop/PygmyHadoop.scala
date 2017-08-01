787bea02b1d1f4394d853e4bb284a0c0fa87ac08
Jan Ouwens <jan.ouwens@gmail.com>
2017-08-01 07:21:45 +0200

    Chapter 9: foldMap implementation and tests

--- src/main/scala/ch09_pygmy_hadoop/PygmyHadoop.scala
+++ src/main/scala/ch09_pygmy_hadoop/PygmyHadoop.scala
@@ -1,8 +1,10 @@
 package ch09_pygmy_hadoop
 
 import cats.Monoid
+import cats.syntax.all._
 
 object PygmyHadoop {
 
-  def foldMap[A, B: Monoid](as: Vector[A])(f: A => B): B = ???
+  def foldMap[A, B: Monoid](as: Vector[A])(f: A => B): B =
+    as.map(f).reduce(_ |+| _)
 }
--- src/test/scala/ch09_pygmy_hadoop/PygmyHadoopTest.scala
+++ src/test/scala/ch09_pygmy_hadoop/PygmyHadoopTest.scala
@@ -1,5 +1,22 @@
 package ch09_pygmy_hadoop
 
-class PygmyHadoopTest {
+import cats.instances.all._
+import ch09_pygmy_hadoop.PygmyHadoop.foldMap
+import org.scalatest.{FlatSpec, Matchers}
 
+class PygmyHadoopTest extends FlatSpec with Matchers {
+
+  behavior of "foldMap"
+
+  it should "calculate a sum using identity" in {
+    foldMap(Vector(1, 2, 3))(identity) should be (6)
+  }
+
+  it should "map to a String and concatenate" in {
+    foldMap(Vector(1, 2, 3))(_.toString + "! ") should be ("1! 2! 3! ")
+  }
+
+  it should "map over a String to produce a String" in {
+    foldMap("Hello world!".toVector)(_.toString.toUpperCase) should be ("HELLO WORLD!")
+  }
 }
