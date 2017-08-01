a09074d173615c52ccd6af12d522202c72b7c8ab
Jan Ouwens <jan.ouwens@gmail.com>
2017-08-01 16:31:09 +0200

    Chapter 9: parallelFoldMap

--- src/main/scala/ch09_pygmy_hadoop/PygmyHadoop.scala
+++ src/main/scala/ch09_pygmy_hadoop/PygmyHadoop.scala
@@ -3,8 +3,20 @@ package ch09_pygmy_hadoop
 import cats.Monoid
 import cats.syntax.all._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 object PygmyHadoop {
 
   def foldMap[A, B: Monoid](as: Vector[A])(f: A => B): B =
     as.map(f).reduce(_ |+| _)
+
+  def parallelFoldMap[A, B: Monoid](as: Vector[A])(f: A => B): Future[B] = {
+    val cpus = Runtime.getRuntime.availableProcessors()
+    val work = as.grouped(cpus)
+    val batches = work.map { w =>
+      Future { foldMap(w)(f) }
+    }
+    Future.sequence(batches).map(_.reduce(_ |+| _))
+  }
 }
--- src/test/scala/ch09_pygmy_hadoop/PygmyHadoopTest.scala
+++ src/test/scala/ch09_pygmy_hadoop/PygmyHadoopTest.scala
@@ -1,10 +1,11 @@
 package ch09_pygmy_hadoop
 
 import cats.instances.all._
-import ch09_pygmy_hadoop.PygmyHadoop.foldMap
+import ch09_pygmy_hadoop.PygmyHadoop._
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FlatSpec, Matchers}
 
-class PygmyHadoopTest extends FlatSpec with Matchers {
+class PygmyHadoopTest extends FlatSpec with Matchers with ScalaFutures {
 
   behavior of "foldMap"
 
@@ -18,5 +19,20 @@ class PygmyHadoopTest extends FlatSpec with Matchers {
 
   it should "map over a String to produce a String" in {
     foldMap("Hello world!".toVector)(_.toString.toUpperCase) should be ("HELLO WORLD!")
+  }
+
+
+  behavior of "parallelFoldMap"
+
+  it should "calculate a sum using identity" in {
+    parallelFoldMap(Vector(1, 2, 3))(identity).futureValue should be (6)
+  }
+
+  it should "map to a String and concatenate" in {
+    parallelFoldMap(Vector(1, 2, 3))(_.toString + "! ").futureValue should be ("1! 2! 3! ")
+  }
+
+  it should "map over a String to produce a String" in {
+    parallelFoldMap("Hello world!".toVector)(_.toString.toUpperCase).futureValue should be ("HELLO WORLD!")
   }
 }
