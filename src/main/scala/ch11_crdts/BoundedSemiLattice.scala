0eaf4e3de82d00f0de3a7982b2c3914752d79a14
Jan Ouwens <jan.ouwens@gmail.com>
2017-08-02 07:38:35 +0200

    Chapter 11: BoundedSemiLattice

---
+++ src/main/scala/ch11_crdts/BoundedSemiLattice.scala
@@ -0,0 +1,20 @@
+package ch11_crdts
+
+import cats.Monoid
+
+trait BoundedSemiLattice[A] extends Monoid[A] {
+  def combine(a1: A, a2: A): A
+  def empty: A
+}
+
+object BoundedSemiLattice {
+  implicit val intBoundedSemiLattice = new BoundedSemiLattice[Int] {
+    override def combine(a1: Int, a2: Int): Int = a1 + a2
+    override def empty: Int = 0
+  }
+
+  implicit def setBoundedSemiLattice[E] = new BoundedSemiLattice[Set[E]] {
+    override def combine(a1: Set[E], a2: Set[E]): Set[E] = a1 union a2
+    override def empty: Set[E] = Set.empty[E]
+  }
+}
